5cbae3179bfa982b86f9cff15a238daf8015da72
LizML06 <pylliz@hotmail.com>
2017-01-12 10:21:00 -0600

    py2

--- calculator.py
+++ calculator.py
@@ -12,5 +12,11 @@ class CalculatorClass(object):
         """
         Your method documentation here
         """
-        # your sum code here
-        return "not implement yet"  # Remove this dummy line
+		#Inicializar la variable en 0
+		resultado = 0
+		#Ciclo para realizar la suma de los elementos que tiene la lista
+		for i in range(0, len(sum_list)):
+			#Suma de los valores que tiene la lista 
+			resultado = resultado + sum_list[i]
+		#Resultado de la suma de los valores de la lista
+		return resultado
