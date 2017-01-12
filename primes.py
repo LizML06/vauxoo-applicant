26c8a2a0b444eaff15f454b0695b8a0636e0810f
LizML06 <pylliz@hotmail.com>
2017-01-12 10:18:20 -0600

    py1

--- primes.py
+++ primes.py
@@ -12,5 +12,15 @@ class PrimeClass(object):
         """
         Your method documentation here
         """
-        # your primes code here
-        return "not implement yet"  # Remove this dummy line
+		#Condición si el numero es menor a 2, no es primo
+		if num_int<2:    
+			#Por lo tanto devuelve Falso
+			return False
+		#Ciclo desde el 2 hasta el numero que escogimos para obtener las divisiones
+		for i in range(2, num_int):  
+			#Condición para ver si el residuo de la división es igual a 0 
+			if num_int%i == 0:   
+				#El número no es primo por lo tanto devuelve falso
+				return False
+		#El numero es primo, devuelve verdadero
+		return True  
