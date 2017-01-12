4ea0f3d589a24cacc14ca42952baf4617365c0af
LizML06 <pylliz@hotmail.com>
2017-01-12 15:28:18 -0600

    pyNumero Primo Corregido

--- primes.py
+++ primes.py
@@ -10,17 +10,18 @@ class PrimeClass(object):
 
     def is_prime(self, num_int):
         """
-        Your method documentation here
+        Metodo que recibe un numero e indica si el valor es
+        primo o no con un True o False
         """
-		#Condición si el numero es menor a 2, no es primo
-		if num_int<2:    
-			#Por lo tanto devuelve Falso
-			return False
-		#Ciclo desde el 2 hasta el numero que escogimos para obtener las divisiones
-		for i in range(2, num_int):  
-			#Condición para ver si el residuo de la división es igual a 0 
-			if num_int%i == 0:   
-				#El número no es primo por lo tanto devuelve falso
-				return False
-		#El numero es primo, devuelve verdadero
-		return True  
+		# Condicion si el numero es menor a 2, no es primo
+        if num_int < 2:    
+            # Por lo tanto devuelve Falso
+            return False
+        # Ciclo desde el 2 hasta el numero que escogimos para obtener las divisiones
+        for incremento in range(2, num_int):  
+            # Condicion para ver si el residuo de la división es igual a 0 
+            if num_int % incremento == 0:   
+                # El número no es primo por lo tanto devuelve falso
+                return False
+        # El numero es primo, devuelve verdadero
+        return True  
