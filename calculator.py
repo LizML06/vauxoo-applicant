3fcc457e0d7cd897824dfa3cfc930afe433cc25c
LizML06 <pylliz@hotmail.com>
2017-01-12 15:25:25 -0600

    pyCalcularSuma

--- calculator.py
+++ calculator.py
@@ -10,13 +10,14 @@ class CalculatorClass(object):
 
     def sum(self, num_list):
         """
-        Your method documentation here
+        Metodo que recibe una lista de numero
+        como entrada y regresa la suma de todos
         """
-		#Inicializar la variable en 0
-		resultado = 0
-		#Ciclo para realizar la suma de los elementos que tiene la lista
-		for i in range(0, len(sum_list)):
-			#Suma de los valores que tiene la lista 
-			resultado = resultado + sum_list[i]
-		#Resultado de la suma de los valores de la lista
-		return resultado
+        # Inicializar la variable en 0
+        resultado = 0
+        # Ciclo para realizar la suma de los elementos que tiene la lista
+        for incremento in range(0 , len(sum_list)):
+            # Suma de los valores que tiene la lista 
+            resultado = resultado + sum_list [incremento]
+        # Resultado de la suma de los valores de la lista
+        return resultado
