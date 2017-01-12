be3c9294332f6877b155bdabadfa7cd41e931586
LizML06 <pylliz@hotmail.com>
2017-01-12 09:12:59 -0600

    sql1

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -4,12 +4,38 @@
 --       Consider add ';' at end sentence.
 
 CREATE TABLE employee (
+	id integer PRIMARY KEY NOT NULL,
+	first_name varchar(50),
+	last_name varchar(50),
+    id_departament integer REFERENCES employee_department
 );
 
 CREATE TABLE employee_department (
+	id integer PRIMARY KEY NOT NULL,
+	name varchar(40),
+	description varchar(100)
 );
 
 CREATE TABLE employee_hobby (
 );
+
+Insert into employee_department (id, name, description) values (1,'Sistemas', 'Departamento del area de Sistemas');
+Insert into employee_department (id,name, description) values (2, 'Industrial', 'Departamento del area de Arquitectura');
+Insert into employee_department (id, name, description) values (3, 'Servicios Escolares', 'Departamento de Servicios Escolares para alumnos');
+Insert into employee_department (id, name, description) values (4, 'Actividades Extraescolares', 'Departamento de planeación');
+Insert into employee_department (id, name, description) values (5, 'Centro de Cómputo', 'Departamento Administrativo');
+Insert into employee_department (id, name, description) values (6, 'Dirección', 'Departamento Directivo');
+
+Insert into employee (id, first_name, last_name, id_departament) values (1, 'Carolina', 'Jimenez', 2);
+Insert into employee (id, first_name, last_name, id_departament) values (2, 'Mario', 'Torres', 1);
+Insert into employee (id, first_name, last_name, id_departament) values (3, 'José', 'Ruiz', 2);
+Insert into employee (id, first_name, last_name, id_departament) values (4, 'Mariana', 'Peréz', 3);
+
+SELECT * FROM employee;
+SELECT * FROM employee_department;
+
+
+
+
 
 -- ...
